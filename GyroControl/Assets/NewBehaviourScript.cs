f13afb6c4794df87333798cdd2bdce3d090eed3a
Xu Han <hanxu_hester@163.com>
2015-12-14 13:40:32 +0100

    Gyro

---
+++ GyroControl/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnGUI() {
+		GUILayout.Label("Magnetometer reading: " + Input.compass.rawVector.ToString());
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
